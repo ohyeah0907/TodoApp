2ddd4ce3f1fd9876dac3ca9a3e919879af52b0aa
TriPham <phamductri2001@gmail.com>
2022-12-16 19:07:47 +0700

    edit lan 8

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 # /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'sqlite:///' + os.path.join(basedir, 'todo.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database/todo.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
